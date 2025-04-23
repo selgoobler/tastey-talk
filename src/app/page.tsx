792bdab125228fc3cce1236c7ab7d354334d9871
Selina Ng <you@example.com>
2025-04-22 21:58:02 -0400

    creating navbar dependent on mobile or website

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,9 @@
-import ModeToggle from "@/components/ModeToggle";
-import { Button } from "@/components/ui/button";
-import {
-  SignInButton,
-  SignUpButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
+
 
 export default function Home() {
   return (
     <div>
-      <SignedOut>
-        <SignInButton mode="modal" />
-        <SignUpButton />
-      </SignedOut>
-      <Button>Button</Button>
-      <ModeToggle />
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+     <h1>Welcome Home!</h1>
     </div>
   );
 }
